58ec1da674289b940780627322d7fae8117892f5
Seth Barlow <sethdbarlow@gmail.com>
2023-01-29 15:39:12 -0700

    all tests passing

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400){
+    return "This one is on me!";
+  } else if (someNumber > 400 && someNumber < 2000){
+    return "That will be twenty bucks.";
+  } else if (someNumber > 2000 && someNumber < 2500){
+    return "I will gladly take your thirty bucks.";
+  } else if (someNumber > 2500) {
+    return "No can do.";
+  }
+
+
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? 'Ok, sounds good.' : 'No go.'; 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmnount){
+  switch (tipAmnount) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye."
+  }
 }
